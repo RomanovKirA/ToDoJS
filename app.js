2b7e4c980c54e99761fc16339a24a5324bf97dd6
RomanovKirA <ka_rom@mail.ru>
2024-02-04 01:39:57 +0300

    complete add to

--- app.js
+++ app.js
@@ -1,66 +1,52 @@
-// Массив тудушек
 let todoItems = [];
 
-
 function renderTodo(todo) {
-  // Выбираем div с классом js-todo-list в котором хранятся тудушки
-  const list = document.querySelector('.js-todo-list');
+  const list = document.querySelector(".js-todo-list");
 
-  // Проверяем, выполненная ли тудушка пришла в аргументе (читай про тернарные операторы)
-  const isChecked = todo.checked ? 'done': '';
-  
-  // Создаем новый див внутри родителя, который будет хранить тудушку
-  const node = document.createElement("div");
-  
-  // Указываем класс для нового дива
-  node.setAttribute('class', `todo-item`);
+  const isChecked = todo.checked ? "done" : "";
 
-  // Устанавливаем id для дива
-  node.setAttribute('id', todo.id);
+  const htmlItem = document.createElement("div");
 
-  // Верстаем контент для дива
-  node.innerHTML = `
-    <input id="${todo.id}" type="checkbox"/>
+  htmlItem.setAttribute("class", `todo-item`);
+
+  htmlItem.setAttribute("id", todo.id);
+
+  htmlItem.innerHTML = `
+    <input class="" id="${todo.id}" type="checkbox"/>
     <label for="${todo.id}" class="tick js-tick"></label>
-    <span>${todo.title}</span>
-    <button class="delete-todo js-delete-todo">
+    <span class='item-text'>${todo.title}</span>
+    <button class="delete-todo js-delete-todo icon-btn">
     <img class="icon" src="./images/full-trash-svgrepo-com.svg" alt="" />
     </button>
   `;
 
-  // Добавляем наш див в общий список тудушек
-  list.append(node);
+  list.prepend(htmlItem);
 }
 
-// Функция добавляет объект todo в массив todoItems
 function addTodo(todoText) {
   const todo = {
     title: todoText,
     checked: false,
-    id: Date.now()
+    id: Date.now(),
   };
 
   todoItems.push(todo);
-  renderTodo(todo)
+  renderTodo(todo);
   console.log(todoItems);
 }
 
-// В HTML div стал form, у button появился тип submit
-const form = document.querySelector('#todo-form');
+const form = document.querySelector("#todo-form");
 
-// Когда происходит клик по button type submit или нажатие на enter, в форме происходит событие submit, которое мы отслеживаем тут
-form.addEventListener('submit', event => {
-  // Предотвращение стандартного поведения, чтобы не перезагружалась страница
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  
-  // Берем инпут, чтобы подтянуть из него значение в переменную text
-  const input = document.querySelector('.todo-container__todo-name');
 
-  // Функция trim() убирает лишние пробелы если они есть, чтобы не было двойных пробелов/кучи пробелов вначале/кучи пробелов в конце
+  const input = document.querySelector(".todo-container__todo-name");
+
   const text = input.value.trim();
 
-  // Если значение инпута не пустое, вызываем функцию addTodo и передаем туда аргументом текст из инпута
   if (text) {
     addTodo(text);
   }
+
+  input.value = "";
 });
