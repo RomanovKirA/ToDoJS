bec565752586336617231445e3e83ef38f254014
RomanovKirA <ka_rom@mail.ru>
2024-04-14 23:41:05 +0300

    wip

--- app.js
+++ app.js
@@ -11,29 +11,72 @@ deleteAllButton.addEventListener("click", function () {
   emptyTodoBlock.style.display = "flex";
 });
 
-todoForm.addEventListener("submit", function (event) {
+todoForm.addEventListener("submit", renderTodo);
+
+todoList.addEventListener("click", deleteTodo);
+
+function renderTodo(event) {
   event.preventDefault();
   if (todoInput.value) {
-    renderTodo();
-    todoInput.value = "";
+    const todoValue = todoInput.value;
+    const todoHtml = `<li class="todo-list__item">
+    <span class="item__text">${todoValue}</span>
+    <button
+    type="button"
+    id="done-todo"
+    class="icon-btn todo-container__btn-add" 
+    >
+    <img class="icon" src="./images/checked-tick-svgrepo-com.svg" alt="" />
+    </button>
+    <button
+    type="button"
+    id="edit-todo"
+    class="icon-btn todo-container__btn-add" 
+    >
+    <img class="icon" src="./images/edit-svgrepo-com (1).svg" alt="" />
+    </button>
+    <button 
+    type='button' 
+    class="icon-btn todo-container__btn-delete" 
+    id="delete-todo" 
+    data-action="delete">
+    <img class="icon" src="./images/eraser-clean-svgrepo-com.svg" alt="" />
+    </button>
+    </li>`;
+    todoList.insertAdjacentHTML("afterbegin", todoHtml);
+    todoInput.focus();
+  }
+  todoInput.value = "";
+  if (todoList.children.length) {
     emptyTodoBlock.style.display = "none";
   }
-});
+}
 
-const renderTodo = function () {
-  const todoText = todoInput.value;
-  const todoHtml = `<li class="todo-list__item">
-        <span class="item__text">${todoText}</span>
-        <button
-          type="submit"
-          id="edit-todo"
-          class="icon-btn todo-container__btn-add"
-        >
-          <img class="icon" src="./images/edit-svgrepo-com (1).svg" alt="" />
-        </button>
-        <button class="icon-btn todo-container__btn-delete" id="delete-todo">
-          <img class="icon" src="./images/eraser-clean-svgrepo-com.svg" alt="" />
-        </button>
-      </li>`;
-  todoList.insertAdjacentHTML("afterbegin", todoHtml);
-};
+function deleteTodo(event) {
+  if (event.target.dataset.action !== "delete") return;
+  const parentNode = event.target.closest(".todo-list__item");
+  parentNode.remove();
+  if (todoList.children.length === 0) {
+    emptyTodoBlock.style.display = "flex";
+  }
+}
+
+let editTodo = document.querySelectorAll("#edit-todo");
+let textTodo = document.querySelectorAll('.item__text')
+
+for (let i = 0; i < editTodo.length; i++) {
+  let editMode = false;
+
+  editTodo[i].addEventListener("click", function () {
+    if (editMode) {
+      this.textContent = "Edit";
+      text[i].removeAttribute("contentEditable");
+    } else {
+      this.textContent = "Ok";
+      text[i].setAttribute("contentEditable", true);
+      text[i].focus();
+    }
+
+    editMode = !editMode;
+  });
+}
